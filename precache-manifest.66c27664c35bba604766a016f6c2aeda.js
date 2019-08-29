f717e51b09f8ea91294ccf0f167d8da580e39826
Janne <janne@Jannes-MacBook-Pro.local>
2019-08-29 13:22:39 +0300

    Updates

---
+++ precache-manifest.66c27664c35bba604766a016f6c2aeda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5038f2794345ec9baa7b",
+    "url": "/beerFrontend/static/js/runtime~main.5038f279.js"
+  },
+  {
+    "revision": "ac7488b9940bf7a7520a",
+    "url": "/beerFrontend/static/js/main.ac7488b9.chunk.js"
+  },
+  {
+    "revision": "5c36158952afa6bed054",
+    "url": "/beerFrontend/static/js/1.5c361589.chunk.js"
+  },
+  {
+    "revision": "ac7488b9940bf7a7520a",
+    "url": "/beerFrontend/static/css/main.a677d341.chunk.css"
+  },
+  {
+    "revision": "cea7df38365865387039cf399a7e2b35",
+    "url": "/beerFrontend/index.html"
+  }
+];
