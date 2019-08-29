47ac285adf0657c3314c44a5c6f3eef95b3488bb
Janne <janne@Jannes-MacBook-Pro.local>
2019-08-29 18:30:12 +0300

    Added more stuff

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -28,9 +28,9 @@ class SearchBar extends Component{
         return (
           <div>
                     <Grid className='test'container direction="row">
-                            <Grid item>
+                            <Grid item item xs={12}md={3}>
                             <TextField style={Object.assign({}, textboxStyle)}placeholder="SEARCH"
-                    onChange={event=>this.onInputChange(event.target.value)}/>&nbsp;
+                                 onChange={event=>this.onInputChange(event.target.value)}/>&nbsp;
                             </Grid>
                             <Grid item>
                             <SearchIcon  />
